593d4078b354aad8002a7a5f30d1252e4cb17c94
Glenn Karlo Manguiat <karlomanguiat@Glenns-MacBook-Pro.local>
2019-02-05 12:36:28 +0800

    First Commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+//import Main from './components/Main';
+import AddForm from './components/AddForm';
+//import t from 'tcomb-form-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <AddForm/>
+    );
+  }
+}
